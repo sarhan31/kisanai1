bfc55c2e4a94a2b50e30b2b8431ee25cca443088
sarhan31 <sarhanvohra31@gmail.com>
2025-09-10 18:23:29 +0530

    sa

--- kisanai/vite.config.mjs
+++ kisanai/vite.config.mjs
@@ -1,21 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
-  },
-  plugins: [tsconfigPaths(), react(), tagger()],
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
-});
+  plugins: [react()],
+  base: "/", // 👈 important for Vercel deployment
+})
